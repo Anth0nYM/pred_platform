982e906049175124818a9fadddcbe84f254b957e
Anth0nYM <marquesanthony62@gmail.com>
2024-06-01 23:02:42 -0300

    feat: capture and preview image feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:pred_platform/home_page.dart';
+import 'package:pred_platform/camera_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: CameraPage(),
     );
   }
 }
